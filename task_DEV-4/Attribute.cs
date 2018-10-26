ccf6a8e1b8a8f37dd46c0c942e52adc553fd06bc
KatsiarynaBukraba <eka.bukraba@gmail.com>
2018-10-26 23:12:33 +0300

    work for project

---
+++ task_DEV-4/Attribute.cs
@@ -0,0 +1,8 @@
+﻿namespace task_DEV_4
+{
+    public class Attribute
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
--- task_DEV-4/Program.cs
+++ task_DEV-4/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace task_DEV_4
+﻿namespace task_DEV_4
 {
     class Program
     {
